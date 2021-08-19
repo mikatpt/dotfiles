48f9b38283ceeed8c54e777c43e9e93b441e8cbc
mikatpt <mikatpt@gmail.com>
2021-08-18 21:21:53 -0400

    modularized install files

--- install.sh
+++ install.sh
@@ -1,40 +1,52 @@
 #!/bin/bash
 
-echo "Checking packages..."
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    if [ $(dpkg-query -W -f='${Status}' apt 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-        echo "Error - please install apt and run this script again."
+install_stow() {
+    echo "Checking packages..."
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        if [ $(dpkg-query -W -f='${Status}' apt 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+            echo "Error - please install apt and run this script again."
+            exit
+        fi
+        sudo apt-get install -y stow
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        which -s brew
+        if [[ $? != 0 ]] ; then
+            echo "Error - please install brew and run this script again."
+            exit
+        fi
+        brew install stow
+    else
+        echo "sorry! this script only works on linux or macos."
         exit
     fi
-    sudo apt-get install -y stow
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    which -s brew
-    if [[ $? != 0 ]] ; then
-        echo "Error - please install brew and run this script again."
-        exit
+}
+
+# Configure dotfiles
+stow_files() {
+    bash $1/configure.sh
+}
+
+install_packages() {
+    echo "Installing packages..."
+    # Order is important here! Setup fish before dev.
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        bash $d/setup/linux/fish.sh
+        bash $d/setup/linux/dev.sh
+        bash $d/setup/linux/nvim.sh
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        bash $d/setup/mac/fish.sh
+        bash $d/setup/mac/dev.sh
+        bash $d/setup/mac/nvim.sh
+    else
+        echo "sorry! this script only works on linux or macos."
     fi
-    brew install stow
-else
-    echo "sorry! this script only works on linux or macos."
-    exit
-fi
+
+}
 
 # Set current directory
 d=$(dirname $(readlink -f $0))
 
-# Configure dotfiles
-bash $d/configure.sh
+install_stow
+stow_files $d
+install_packages
 
-echo "Installing packages..."
-# Order is important here! Setup fish before dev.
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    bash $d/setup/linux/fish.sh
-    bash $d/setup/linux/dev.sh
-    bash $d/setup/linux/nvim.sh
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    bash $d/setup/mac/fish.sh
-    bash $d/setup/mac/dev.sh
-    bash $d/setup/mac/nvim.sh
-else
-    echo "sorry! this script only works on linux or macos."
-fi
--- setup/mac/nvim.sh
+++ setup/mac/nvim.sh
@@ -1,27 +1,35 @@
 #!/usr/bin/bash
 
-# Install neovim
-brew install --HEAD luajit
-brew install --HEAD neovim
+install_neovim() {
+    brew install --HEAD luajit
+    brew install --HEAD neovim
+}
 
-# Setup neovim
-nvim --headless +PackerInstall +qall
-nvim --headless +"LspInstall bash" +"LspInstall lua" +"LspInstall vim" +"LspInstall efm" +qall
+setup_neovim() {
+    nvim --headless +PackerInstall +qall
+    nvim --headless +"LspInstall bash" +"LspInstall lua" +"LspInstall vim" +"LspInstall efm" +qall
+}
 
-# Language formatters
-npm i -g eslint_d prettier
+configure_language_servers() {
+    # Language formatters
+    npm i -g eslint_d prettier
 
-# Go debugging
-mkdir -p ~/.debug
-cd ~/.debug
-git clone https://github.com/golang/vscode-go
-cd ~/.debug/vscode-go
-npm i
-npm run compile
+    # Go debugging
+    mkdir -p ~/.debug
+    cd ~/.debug
+    git clone https://github.com/golang/vscode-go
+    cd ~/.debug/vscode-go
+    npm i
+    npm run compile
 
-# JavaScript debugging
-cd ~/.debug
-git clone https://github.com/microsoft/vscode-node-debug2.git
-cd ~/.debug/vscode-node-debug2
-npm i
-npm run build
+    # JavaScript debugging
+    cd ~/.debug
+    git clone https://github.com/microsoft/vscode-node-debug2.git
+    cd ~/.debug/vscode-node-debug2
+    npm i
+    npm run build
+}
+
+install_neovim
+setup_neovim
+configure_language_servers
