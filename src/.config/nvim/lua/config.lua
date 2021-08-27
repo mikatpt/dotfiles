ea0f92d3f4dc67637cafca36e220470f28a222d8
mikatpt <mikatpt@gmail.com>
2021-08-27 01:18:48 -0400

    Updated dashboard to start more often

--- src/.config/nvim/lua/config.lua
+++ src/.config/nvim/lua/config.lua
@@ -1,5 +1,12 @@
 require('modules')
 
-if vim.api.nvim_buf_get_name(0):len() == 0 then
+local buffer_name = vim.api.nvim_buf_name(0)
+local no_file_open = buffer_name:len() == 0
+local is_dir = vim.fn.isdirectory(buffer_name)
+
+if no_file_open or is_dir then
+    vim.cmd('silent! PackerLoad nvim-tree.lua')
+    vim.cmd('silent! PackerLoad dashboard-nvim')
     vim.cmd('silent! Dashboard')
+    vim.cmd('silent! NvimTreeOpen')
 end
