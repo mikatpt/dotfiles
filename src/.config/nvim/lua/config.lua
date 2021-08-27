19e0509fbcd39a6ba46fffa94f1ac87505e3420c
mikatpt <mikatpt@gmail.com>
2021-08-27 02:23:06 -0400

    fixed some startup lag issues and nvim-tree stuff

--- src/.config/nvim/lua/config.lua
+++ src/.config/nvim/lua/config.lua
@@ -1,12 +1,7 @@
 require('modules')
 
-local buffer_name = vim.api.nvim_buf_name(0)
-local no_file_open = buffer_name:len() == 0
-local is_dir = vim.fn.isdirectory(buffer_name)
+local empty_buffer = vim.api.nvim_buf_get_name(0):len() == 0
 
-if no_file_open or is_dir then
-    vim.cmd('silent! PackerLoad nvim-tree.lua')
-    vim.cmd('silent! PackerLoad dashboard-nvim')
+if empty_buffer then
     vim.cmd('silent! Dashboard')
-    vim.cmd('silent! NvimTreeOpen')
 end
--- src/.config/nvim/lua/core/utils.lua
+++ src/.config/nvim/lua/core/utils.lua
@@ -4,6 +4,14 @@ function P(cmd)
     print(vim.inspect(cmd))
 end
 
+local function is_git_dir()
+    if os.execute('git rev-parse --is-inside-work-tree >> /dev/null 2>&1') == 0 then
+        return 1
+    else
+        return 0
+    end
+end
+
 -- Os
 M.os = {
     home = os.getenv('HOME'),
@@ -11,9 +19,7 @@ M.os = {
     cache = vim.fn.stdpath('cache'),
     config = vim.fn.stdpath('config'),
     name = vim.loop.os_uname().sysname,
-    is_git_dir = os.execute(
-        'git rev-parse --is-inside-work-tree >> /dev/null 2>&1'
-    ),
+    is_git_dir = is_git_dir,
 }
 
 return M
