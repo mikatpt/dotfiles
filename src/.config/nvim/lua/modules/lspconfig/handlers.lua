19fb8d61664d758edc545eb8eebb2b4caf4674b0
mikatpt <mikatpt@gmail.com>
2021-09-05 21:52:08 -0400

    adjust jump to implementation

---
+++ src/.config/nvim/lua/modules/lspconfig/handlers.lua
@@ -0,0 +1,32 @@
+local M = {}
+
+-- Do not include implementations from files including 'mocks'
+M.implementation = function()
+
+    local params = vim.lsp.util.make_position_params()
+
+    vim.lsp.buf_request(0, 'textDocument/implementation', params, function(err, method, result, client_id, bufnr, config)
+        local ft = vim.api.nvim_buf_get_option(bufnr, 'filetype')
+        if ft == 'go' then
+            local new_result = vim.tbl_filter(function(v)
+                return not string.find(v.uri, 'mocks')
+            end, result)
+            if #new_result > 0 then
+                result = new_result
+            end
+
+            if #new_result > 1 then
+                require'telescope.builtin'.lsp_implementations({
+                    layout_strategy = 'vertical',
+                    file_ignore_patterns = { '*mock*/*', '**/*mock*', '*test*/*', '**/*test*' }
+                })
+                return
+            end
+        end
+
+        vim.lsp.handlers['textDocument/implementation'](err, method, result, client_id, bufnr, config)
+        vim.cmd [[normal! zz]]
+    end)
+end
+
+return M
