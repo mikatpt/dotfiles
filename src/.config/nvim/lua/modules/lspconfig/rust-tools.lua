de86e9b49e9022ee9f70cc681d233a1dc9a6f3b1
mikatpt <mikatpt@gmail.com>
2021-09-28 10:05:30 -0400

    revert formatoptions, fix rust tools format

--- src/.config/nvim/lua/modules/lspconfig/rust-tools.lua
+++ src/.config/nvim/lua/modules/lspconfig/rust-tools.lua
@@ -1,14 +1,16 @@
 return function()
     require'rust-tools'.setup({
-        hover_actions = {
-            border = 'single',
-            auto_focus = false
+        tools = {
+            hover_actions = {
+                border = 'single',
+                auto_focus = false
+            },
+            inlay_hints = {
+                highlight = "TSText",
+            }
         },
         server = {
             on_attach = require('modules.lspconfig.on-attach'),
         },
-        inlay_hints = {
-            highlight = "TSText",
-        }
     })
 end
