cd6310bca88ed46d21c8f746ce9c7e2dc74af528
mikatpt <mikatpt@gmail.com>
2021-10-04 00:28:10 -0400

    Add gx mapping

--- src/.config/nvim/init.lua
+++ src/.config/nvim/init.lua
@@ -7,3 +7,7 @@ local empty_buffer = vim.api.nvim_buf_get_name(0):len() == 0
 if empty_buffer then
     vim.cmd('silent! Dashboard')
 end
+
+local cmd = ':!xdg-open <cWORD> &<CR><CR>'
+if require'core.utils'.os.name == 'Darwin' then cmd = ':!open <cWORD> &<CR><CR>' end
+vim.api.nvim_set_keymap('n', 'gx', cmd, { silent = false, noremap = true})
