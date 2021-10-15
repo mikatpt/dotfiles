50e21392ad3077fbe80523ff100c5e35a30e406a
Michael Chen <mikatpt@gmail.com>
2021-10-15 15:43:33 -0400

    Move some keybindings out

--- src/.alias.zsh
+++ src/.alias.zsh
@@ -50,7 +50,6 @@ alias ....='cd ../../..'
 alias ffs='sudo !!'
 alias :q='exit'
 
-alias bazeltest='bazel test --test_verbose_timeout_warnings --test_output errors --test_summary detailed ...:all'
 alias cm='git commit'
 alias ga='git add'
 alias gp='git push'
@@ -66,13 +65,6 @@ alias sd='git stash drop'
 alias sp='git stash pop'
 alias sa='git stash apply'
 alias sl='git stash list'
-
-alias fco='fuzzyhub checkout'        # checkout a branch. Specifying branch name will use git checkout instead.
-alias far='fuzzyhub add-remote'      # add git remote
-alias fsm='fuzzyhub sync-master'     # sync local, origin and upstream master
-alias fpr='fuzzyhub view-pr'         # view pull request(s) on github
-alias fm='fuzzyhub view-master'      # view master on github. ctrl-t to specify file(s)
-alias fl='fuzzyhub view-local'       # view local branch on github. ctrl-t to specify file(s)
 
 alias pingg='ping www.google.com'    # See network speed against google.com
 alias myip="ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'"
