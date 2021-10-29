0063892af599d32e7e6ed79deaa5b23509c5ec48
mikatpt <mikatpt@gmail.com>
2021-10-29 01:50:29 -0400

    Update cmp and autopairs

--- src/.config/nvim/lua/modules/config/autopairs.lua
+++ src/.config/nvim/lua/modules/config/autopairs.lua
@@ -2,9 +2,7 @@ return function()
     require('nvim-autopairs').setup({
         disable_filetype = { 'TelescopePrompt', 'vim' },
     })
-    require('nvim-autopairs.completion.cmp').setup({
-        map_cr = true, --  map <CR> on insert mode
-        map_complete = true, -- it will auto insert `(` after select function or method item
-        auto_select = false, -- auto select first item
-    })
+    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+    local cmp = require('cmp')
+    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
 end
