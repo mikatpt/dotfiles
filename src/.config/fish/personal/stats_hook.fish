ba03816cc20aac6406f1a3aff60f16d66dcb0662
mikatpt <mikatpt@gmail.com>
2022-02-18 19:38:38 -0500

    fix home scripts

--- src/.config/fish/personal/stats_hook.fish
+++ src/.config/fish/personal/stats_hook.fish
@@ -1,4 +1,7 @@
 function testing -e fish_postexec
+    # {"command": "argument_here"}
     set -l CMD (jq -n --arg command "$argv" '{$command}')
-    curl --data "$CMD" --header 'Content-Type: application/json' http://localhost:7749/stats
+
+    # Silent curl to home server
+    curl -s --data "$CMD" --header 'Content-Type: application/json' http://localhost:7749/stats
 end
--- src/.home.fish
+++ src/.home.fish
@@ -1,11 +1,4 @@
-function check_tmux
-    tmux has-session -t home 2>/dev/null
-    if test $status -eq 0
-        exit
-    end
-end
-
-function check_postgres
+function _check_postgres
     set -l POSTGRES_ONLINE (service postgresql status | string match -r 'online')
     if test -z $POSTGRES_ONLINE
         echo "Postgres is not online! Please start postgres and restart terminal with 'exec fish'"
@@ -13,19 +6,22 @@ function check_postgres
     end
 end
 
-function start_server
+function _start_home_server
+    tmux has-session -t home 2>/dev/null
+    if test $status -eq 0
+        return 1
+    end
+
     echo "Initializing home server"
     tmux new-session -d -s home
     tmux send-keys -t home.1 "cd /home/mikatpt/coding/stats/home-server" ENTER
     tmux send-keys -t home.1 "cargo run --release" ENTER
 end
 
-function apply_stats_hook
+function _apply_stats_hook
     echo "Applying stats hook"
     source ~/.config/fish/personal/stats_hook.fish
 end
 
-check_tmux
-check_postgres
-start_server
-apply_stats_hook
+_check_postgres
+_start_home_server
