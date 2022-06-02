c03c27bfdb3dd4b4117133d4de7c0c0edd195bd0
mikatpt <mikatpt@gmail.com>
2022-06-02 04:31:09 -0400

    fix

--- src/.config/nvim/lua/modules/config/dashboard.lua
+++ src/.config/nvim/lua/modules/config/dashboard.lua
@@ -1,5 +1,8 @@
 return function()
-    local alpha = require('alpha')
+    local ok, alpha = pcall(require, 'alpha')
+    if not ok then
+        return
+    end
     local dashboard = require('alpha.themes.dashboard')
     dashboard.section.header.val = {
         '',
@@ -13,29 +16,21 @@ return function()
     }
     dashboard.section.buttons.val = {
         dashboard.button('CTL n', '" Open Tree', ':NvimTreeToggle<CR>', {}),
+        dashboard.button('SPC o', '  Recently opened', ':lua require"telescope.builtin".oldfiles()<CR>', {}),
+        dashboard.button('CTL p', '  Git files', ':silent! lua require"telescope.builtin".git_files()<CR>', {}),
+        dashboard.button(
+            'SPC r',
+            '  Find word',
+            ':lua require"telescope.builtin".grep_string({ search = vim.fn.input("Grep For > ")})<CR>',
+            {}
+        ),
+        dashboard.button(
+            'SPC z',
+            '  Config files',
+            ':lua require"telescope.builtin".git_files({cwd = "$HOME/.config/nvim" })<CR>',
+            {}
+        ),
     }
     dashboard.config.opts.noautocmd = true
     alpha.setup(dashboard.config)
-    -- vim.g.dashboard_custom_section = {
-    --     a = {
-    --         description = { '" Open Tree                            CTL n' },
-    --         command = ':NvimTreeToggle'
-    --     },
-    --     b = {
-    --         description = { '  Recently opened                      SPC o' },
-    --         command = 'lua require"telescope.builtin".oldfiles()',
-    --     },
-    --     c = {
-    --         description = { '  Git files                            CTL p' },
-    --         command = 'silent! lua require"telescope.builtin".git_files()',
-    --     },
-    --     d = {
-    --         description = { '  Find word                            SPC r' },
-    --         command = 'lua require"telescope.builtin".grep_string({ search = vim.fn.input("Grep For > ")})',
-    --     },
-    --     e = {
-    --         description = { '  Config files                         SPC z' },
-    --         command = 'lua require"telescope.builtin".git_files({cwd = "$HOME/.config/nvim" })',
-    --     }
-    -- }
 end
