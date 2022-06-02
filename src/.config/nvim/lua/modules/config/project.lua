23f80be157ad0aa15d92d19c79249de03b9c0dfe
Michael Chen <mikatpt@gmail.com>
2022-06-02 04:40:25 -0400

    fix rooter

--- src/.config/nvim/lua/modules/config/project.lua
+++ src/.config/nvim/lua/modules/config/project.lua
@@ -1,5 +1,6 @@
 return function()
     require('project_nvim').setup({
         patterns = { '.git', '_darcs', '.hg', '.bzr', '.svn', 'package.json', 'Cargo.toml' },
+        detection_methods = { 'pattern' },
     })
 end
