50eda91f52e71deb034013c9bcf6d97ec1fafa80
mikatpt <mikatpt@gmail.com>
2022-06-17 02:28:52 -0400

    update to use vim.keymap

--- src/.config/nvim/lua/core/map.lua
+++ src/.config/nvim/lua/core/map.lua
@@ -3,10 +3,14 @@ local utils = require('core.utils')
 local isDarwin = utils.os.name == 'Darwin'
 local open_cmd = isDarwin and ':!open <cWORD> &<CR><CR>' or ':!xdg-open <cWORD> &<CR><CR>'
 
-local nonsilent = { silent = false, noremap = true }
+local nonsilent = { silent = false }
 
-local map = function(mode, lhs, rhs, opts)
-    vim.api.nvim_set_keymap(mode, lhs, rhs, opts or { silent = true, noremap = true })
+local bind = function(mode, outer_opts)
+    outer_opts = outer_opts or { silent = true }
+    return function(lhs, rhs, opts)
+        opts = vim.tbl_extend('force', outer_opts, opts or {})
+        vim.keymap.set(mode, lhs, rhs, opts)
+    end
 end
 
 local map_lsp = function(mode, lhs, rhs, opts)
@@ -14,126 +18,141 @@ local map_lsp = function(mode, lhs, rhs, opts)
     vim.api.nvim_set_keymap(mode, lhs, rhs .. redraw, opts or { silent = true, noremap = true})
 end
 
+-- :h map-listing
+-- local nmap = bind('n', { silent = true, noremap = false })
+-- local xnoremap = bind('x')
+local nnoremap = bind('n')
+local vnoremap = bind('v')
+local inoremap = bind('i')
+local cnoremap = bind('c')
+local icnoremap = bind('!')
+
 vim.g.mapleader = ' '
-map('i', 'jk',                 '<ESC>')
-map('n', 'Y',                  'y$')
-map('n', 'Q',                  '@@')
-map('n', '<F1>',               '<nop>')
+inoremap('jk',            '<ESC>')
+nnoremap('Y',             'y$')
+nnoremap('Q',             '@@')
+nnoremap('<F1>',          '<nop>')
 
 -- Magic search
-map('n', '?',                  '?\\v')
-map('n', '/',                  '/\\v')
-map('c', '%s/',                '%sm/')
+nnoremap('?',             '?\\v')
+nnoremap('/',             '/\\v')
+cnoremap('%s/',           '%sm/')
+
+-- terminal bindings for command line mode
+cnoremap('<C-A>',         '<Home>',  nonsilent)
+cnoremap('<C-E>',         '<End>',   nonsilent)
+cnoremap('<C-B>',         '<Left>',  nonsilent)
+cnoremap('<C-F>',         '<Right>', nonsilent)
+vim.cmd('set cedit=<C-G>')
 
 -- Merge lines
-map('n', '<S-M>',              '<S-J>')
+nnoremap('<S-M>',         '<S-J>')
 
 -- Use J and K to move lines up and down.
-map('n', '<S-K>',              ':m .-2<CR>==')
-map('n', '<S-J>',              ':m .+1<CR>==')
-map('v', 'J',                  ":m '>+1<CR>gv=gv")
-map('v', 'K',                  ":m '<-2<CR>gv=gv")
+nnoremap('<S-K>',         ':m .-2<CR>==')
+nnoremap('<S-J>',         ':m .+1<CR>==')
+vnoremap('J',             ":m '>+1<CR>gv=gv")
+vnoremap('K',             ":m '<-2<CR>gv=gv")
 
 -- Indenting won't throw you out of visual mode
-map('v', '<LT>',               '<LT>gv')
-map('v', '>',                  '>gv')
+vnoremap('<LT>',          '<LT>gv')
+vnoremap('>',             '>gv')
 
 -- Fix ctrl backspace behavior
-map('!', '<C-BS>',             '<C-W>')
-map('!', '<C-H>',              '<C-W>')
+icnoremap('<C-BS>',       '<C-W>')
+icnoremap('<C-H>',        '<C-W>')
 
 -- misc convenience stuff.
-map_lsp('i', '<C-J>',          '<Esc>o')
-map_lsp('i', '<C-CR>',         '<Esc><S-O>')
-map_lsp('n', '<C-G>',          'A;<Esc>') -- C-G is equivalent to <C-'>
-map_lsp('i', '<C-G>',          '<Esc>A;')
+map_lsp('i', '<C-J>',     '<Esc>o')
+map_lsp('i', '<C-CR>',    '<Esc><S-O>')
+map_lsp('n', '<C-G>',     'A;<Esc>') -- C-G is equivalent to <C-'>
+map_lsp('i', '<C-G>',     '<Esc>A;')
 
 -- From vim-commentary:
 -- gc to comment out lines
 
 -- Resize splits
-map('n', '<S-LEFT>',           ':vert res -5<CR>')
-map('n', '<S-RIGHT>',          ':vert res +5<CR>')
-map('n', '<S-UP>',             ':res +5<CR>')
-map('n', '<S-DOWN>',           ':res -5<CR>')
+nnoremap('<S-LEFT>',      ':vert res -5<CR>')
+nnoremap('<S-RIGHT>',     ':vert res +5<CR>')
+nnoremap('<S-UP>',        ':res +5<CR>')
+nnoremap('<S-DOWN>',      ':res -5<CR>')
 
 -- Use space j or k to open new splits, and Ctrl hjkl to move between them.
-map('n', '<leader>j',          ':split<CR>',           nonsilent)
-map('n', '<leader>k',          ':vsp<CR>',             nonsilent)
-map('n', '<leader>u',          ':sp<CR>',              nonsilent)
-map('n', '<leader><S-J>',      ':topleft vsp<CR>',     nonsilent)
-map('n', '<leader><S-K>',      ':vsp<CR>',             nonsilent)
-map('n', '<C-J>',              '<C-W><S-W>')
-map('n', '<C-K>',              '<C-W>w')
-map('n', '<C-H>',              '<C-W>h')
-map('n', '<C-L>',              '<C-W>l')
+nnoremap('<leader>j',     ':split<CR>',           nonsilent)
+nnoremap('<leader>k',     ':vsp<CR>',             nonsilent)
+nnoremap('<leader>u',     ':sp<CR>',              nonsilent)
+nnoremap('<leader><S-J>', ':topleft vsp<CR>',     nonsilent)
+nnoremap('<leader><S-K>', ':vsp<CR>',             nonsilent)
+nnoremap('<C-J>',         '<C-W><S-W>')
+nnoremap('<C-K>',         '<C-W>w')
+nnoremap('<C-H>',         '<C-W>h')
+nnoremap('<C-L>',         '<C-W>l')
 
 -- File Tree
-map('n', '<C-N>',              ':NvimTreeToggle<CR>')
-map('n', '<leader>nf',         ':NvimTreeFindFile<CR>')
-map('n', '<leader>nr',         ':NvimTreeRefresh<CR>', nonsilent)
-
--- Reload neovim configuration and LSP
-map('n', '<leader>rl',         ':lua require"core.utils".fn.reload_lsp()<CR>')
-map('n', '<leader>rr',         ':lua require"core.utils".fn.reload_config()<CR>', nonsilent)
+nnoremap('<C-N>',         ':NvimTreeToggle<CR>')
+nnoremap('<leader>nf',    ':NvimTreeFindFile<CR>')
+nnoremap('<leader>nr',    ':NvimTreeRefresh<CR>', nonsilent)
 
 -- set current working directory to current file
-map('n', '<leader>cd',         ':cd %:p:h<CR>',        nonsilent)
+nnoremap('<leader>cd',    ':cd %:p:h<CR>',        nonsilent)
 
 -- Toggle relative/absolute line numbers
-map('n', '<leader>l',          ':set rnu!<CR>',        nonsilent)
+nnoremap('<leader>l',     ':set rnu!<CR>',        nonsilent)
 
 -- Yank to system clipboard
-map('n', '<leader>y',          '"+y')
-map('v', '<leader>y',          '"+y')
-map('n', '<leader><S-Y>',      '<CMD>%y+<CR>')
+nnoremap('<leader>y',     '"+y')
+vnoremap('<leader>y',     '"+y')
+nnoremap('<leader><S-Y>', '<CMD>%y+<CR>')
 
 -- Open URL
-map('n', 'gx',                 open_cmd,               nonsilent)
+nnoremap('gx',            open_cmd,               nonsilent)
 
 -- Open in Github
-map('n', '<leader>go',         '<CMD>GBrowse<CR>')
+nnoremap('<leader>go',    '<CMD>GBrowse<CR>')
 -- Git merge
-map('n', '<leader>hj',         '<cmd>diffget //2<CR>' )
-map('n', '<leader>hk',         '<cmd>diffget //3<CR>' )
+nnoremap('<leader>hj',    '<CMD>diffget //2<CR>' )
+nnoremap('<leader>hk',    '<CMD>diffget //3<CR>' )
+
+-- Reload neovim configuration and LSP
+nnoremap('<leader>rl',         function() require('core.utils').fn.reload_lsp() end)
+nnoremap('<leader>rr',         function() require('core.utils').fn.reload_config() end, nonsilent)
 
 -- Harpoon
-map('n', '<leader><S-P><S-M>', '<CMD>lua require"harpoon-finder.mark".toggle_dir()<CR>')
-map('n', '<leader><S-P><S-N>', '<CMD>lua require"harpoon-finder.ui".toggle_quick_menu()<CR>')
-map('n', '<leader>p',          '<CMD>lua require"harpoon-finder.ui".find_files()<CR>')
-map('n', '<leader><S-F>',      '<CMD>lua require"telescope.builtin".live_grep({ prompt_title = "Find Text", layout_strategy = "vertical", search_dirs = require"harpoon-finder.ui".get_search_dirs() })<CR>')
+nnoremap('<leader><S-P><S-M>', function() require('harpoon-finder.mark').toggle_dir() end)
+nnoremap('<leader><S-P><S-N>', function() require('harpoon-finder.ui').toggle_quick_menu() end)
+nnoremap('<leader>p',          function() require('harpoon-finder.ui').find_files() end)
+nnoremap('<leader><S-F>',      function() require('telescope.builtin').live_grep({ prompt_title = 'Find Text', layout_strategy = 'vertical', search_dirs = require('harpoon-finder.ui').get_search_dirs() }) end)
 
-map('n', '<leader>m',          ':lua require("harpoon.mark").toggle_file()<CR>')
-map('n', '<leader><S-M>',      ':lua require("harpoon.ui").toggle_quick_menu()<CR>')
+nnoremap('<leader>m',          function() require('harpoon.mark').toggle_file() end)
+nnoremap('<leader><S-M>',      function() require('harpoon.ui').toggle_quick_menu() end)
 for i = 1, 9 do
-    map('n', '<leader>' .. i,  ':lua require("harpoon.ui").nav_file(' .. i .. ')<CR>')
+    nnoremap('<leader>' .. i,  function() require('harpoon.ui').nav_file(i) end)
 end
 
 -- Telescope
 local telescope_cmd = utils.fn.is_git_dir() and 'git_files' or 'find_files'
-map('n', '<C-P>',              '<CMD>Telescope ' .. telescope_cmd .. '<CR>')
-local config = utils.os.config
+nnoremap('<C-P>',              '<CMD>Telescope ' .. telescope_cmd .. '<CR>')
 
-map('n', '<leader>o',          '<CMD>Telescope oldfiles file_ignore_patterns={}<CR>')
-map('n', '<leader>z',          '<CMD>lua require"telescope.builtin".git_files({ prompt_title = "Dotfiles", cwd = "' .. config .. '", file_ignore_patterns = {} })<CR>')
-map('n', '<leader>s',          '<CMD>lua require"telescope.builtin".lsp_dynamic_workspace_symbols({ prompt_title = "Search Symbols", layout_strategy = "vertical" })<CR>')
-map('n', '<leader>b',          '<CMD>lua require"telescope.builtin".buffers()<CR>')
-map('n', '<leader>q',          '<CMD>TroubleToggle document_diagnostics<CR>')
-map('n', '<leader>Q',          '<CMD>TroubleToggle workspace_diagnostics<CR>')
+nnoremap('<leader>o',          '<CMD>Telescope oldfiles file_ignore_patterns={}<CR>')
+nnoremap('<leader>z',          function() require('telescope.builtin').git_files({ prompt_title = 'Dotfiles', cwd = utils.os.config, file_ignore_patterns = {} }) end)
+nnoremap('<leader>s',          function() require('telescope.builtin').lsp_dynamic_workspace_symbols({ prompt_title = 'Search Symbols', layout_strategy = 'vertical' }) end)
+nnoremap('<leader>b',          function() require('telescope.builtin').buffers() end)
+nnoremap('<leader>q',          '<CMD>TroubleToggle document_diagnostics<CR>')
+nnoremap('<leader>Q',          '<CMD>TroubleToggle workspace_diagnostics<CR>')
 
 -- Ripgrep for input or current word
-map('n', '<leader>f',          '<CMD>lua require("telescope.builtin").live_grep({ prompt_title = "Find Text", layout_strategy = "vertical" })<CR>')
-map('n', '<leader>w',          '<CMD>TodoTelescope layout_strategy=vertical<CR>')
+nnoremap('<leader>f',          function() require('telescope.builtin').live_grep({ prompt_title = 'Find Text', layout_strategy = 'vertical' }) end)
+nnoremap('<leader>w',          '<CMD>TodoTelescope layout_strategy=vertical<CR>')
 
 -- DB
-map('n', '<leader><S-D>',      '<CMD>DBUIToggle<CR>')
+nnoremap('<leader><S-D>',      '<CMD>DBUIToggle<CR>')
 
 -- Debugging
-map('n', '<F5>',               ':lua require("dapui").open()<CR>:lua require"dap".continue()<CR>')
-map('n', '<F10>',              ':lua require"dap".step_over()<CR>')
-map('n', '<F11>',              ':lua require"dap".step_into()<CR>')
-map('n', '<F12>',              ':lua require"dap".step_out()<CR>')
-map('n', '<C-Y>',              ':lua require("dapui").toggle()<CR>')
-map('n', '<leader>d',          ':lua require("dapui").eval()<CR>')
-map('v', '<leader>d',          ':lua require("dapui").eval()<CR>')
-map('n', '<C-B>',              ':lua require"dap".toggle_breakpoint()<CR>')
+nnoremap('<F5>',               function() require('dapui').open() end)
+nnoremap('<F10>',              function() require('dap').step_over({}) end)
+nnoremap('<F11>',              function() require('dap').step_into() end)
+nnoremap('<F12>',              function() require('dap').step_out() end)
+nnoremap('<C-Y>',              function() require('dapui').toggle() end)
+nnoremap('<leader>d',          function() require('dapui').eval() end)
+vnoremap('<leader>d',          function() require('dapui').eval() end)
+nnoremap('<C-B>',              function() require('dap').toggle_breakpoint() end)
