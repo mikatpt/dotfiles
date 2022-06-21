23f3870bfee536135a42f9ff553ecb46586a33e8
Michael Chen <mikatpt@gmail.com>
2022-06-21 17:06:35 -0400

    deprecations

--- src/.config/nvim/lua/modules/config/dapui.lua
+++ src/.config/nvim/lua/modules/config/dapui.lua
@@ -9,25 +9,26 @@ return function()
             edit = 'e',
             repl = 'r',
         },
-        sidebar = {
-            -- You can change the order of elements in the sidebar
-            elements = {
-                -- Provide as ID strings or tables with "id" and "size" keys
-                { id = 'breakpoints', size = 0.25 },
-                {
-                    id = 'scopes',
-                    size = 0.5, -- Can be float or integer > 1
+        layouts = {
+            {
+                elements = {
+                    -- Provide as ID strings or tables with "id" and "size" keys
+                    { id = 'breakpoints', size = 0.25 },
+                    {
+                        id = 'scopes',
+                        size = 0.5, -- Can be float or integer > 1
+                    },
+                    { id = 'watches', size = 0.25 },
+                    -- { id = 'stacks', size = 0.25 },
                 },
-                { id = 'watches', size = 0.25 },
-                -- { id = 'stacks', size = 0.25 },
+                size = 40,
+                position = 'right', -- Can be "left", "right", "top", "bottom"
             },
-            size = 40,
-            position = 'right', -- Can be "left", "right", "top", "bottom"
-        },
-        tray = {
-            elements = { 'repl' },
-            size = 10,
-            position = 'bottom', -- Can be "left", "right", "top", "bottom"
+            {
+                elements = { 'repl', 'console' },
+                size = 10,
+                position = 'bottom', -- Can be "left", "right", "top", "bottom"
+            },
         },
         floating = {
             max_height = nil, -- These can be integers or a float between 0 and 1.
