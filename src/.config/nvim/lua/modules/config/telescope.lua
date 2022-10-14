6f3c0e13df5ee32a91f1d1d9fdfb97d855bc5968
mikatpt <mikatpt@gmail.com>
2022-10-13 22:59:39 -0400

    fzf native?

--- src/.config/nvim/lua/modules/config/telescope.lua
+++ src/.config/nvim/lua/modules/config/telescope.lua
@@ -63,6 +63,14 @@ return function()
                 end)
             end,
         },
+        extensions = {
+            fzf = {
+                fuzzy = true,
+                override_generic_sorter = true,
+                override_file_sorter = true,
+                case_mode = 'smart_case',
+            },
+        },
     })
     require('telescope').load_extension('fzf')
 end
