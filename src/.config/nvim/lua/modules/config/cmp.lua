1c9e8d84ec3bdcfd89ca83d2cdd57bf0130991c8
mikatpt <mikatpt@gmail.com>
2022-11-10 12:02:00 -0500

    luasnip weirdness

--- src/.config/nvim/lua/modules/config/cmp.lua
+++ src/.config/nvim/lua/modules/config/cmp.lua
@@ -22,11 +22,6 @@ return function()
         end
     end
 
-    local has_words_before = function()
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-    end
-
     local luasnip = require('luasnip')
 
     require('luasnip.loaders.from_vscode').lazy_load({ paths = './snippets' })
@@ -72,8 +67,6 @@ return function()
                     cmp.select_next_item()
                 elseif luasnip.expand_or_jumpable() then
                     luasnip.expand_or_jump()
-                elseif has_words_before() then
-                    cmp.complete()
                 else
                     fallback()
                 end
