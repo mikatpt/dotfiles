b2a30629ee2bdea637cc1b29d99a7acb09641cf7
Michael Chen <mikatpt@gmail.com>
2023-07-19 04:42:20 -0400

    Add fidget

--- src/.config/nvim/lua/modules/config/feline.lua
+++ src/.config/nvim/lua/modules/config/feline.lua
@@ -273,45 +273,8 @@ return function()
         hl = { fg = colors.statusline_bg, bg = colors.statusline_bg },
     })
 
-    -- LSP info
-    table.insert(components.active[2], {
-        provider = function()
-            local Lsp = vim.lsp.util.get_progress_messages()[1]
-            if Lsp then
-                local msg = Lsp.message and Lsp.message .. ' ' or ''
-                local percentage = Lsp.percentage or 0
-                local title = Lsp.title and Lsp.title .. ' ' or ''
-                local spinners = { '', '', '' }
-
-                local success_icon = { '', '', '' }
-
-                local ms = vim.loop.hrtime() / 1000000
-                local frame = math.floor(ms / 120) % #spinners
-
-                if percentage >= 70 then
-                    return string.format(' %%<%s %s%s(%s%%%%) ', success_icon[frame + 1], title, msg, percentage)
-                else
-                    return string.format(' %%<%s %s%s(%s%%%%) ', spinners[frame + 1], title, msg, percentage)
-                end
-            end
-            return ''
-        end,
-        short_provider = function()
-            local Lsp = vim.lsp.util.get_progress_messages()[1]
-            if Lsp then
-                local msg = Lsp.message and Lsp.message .. ' ' or ''
-                local percentage = Lsp.percentage or 0
-                local title = Lsp.title and Lsp.title .. ' ' or ''
-
-                return string.format(' %%<%s%s(%s%%%%) ', title, msg, percentage)
-            end
-            return ''
-
-        end,
-        left_sep = { str = '', fg = colors.statusline_bg, bg = colors.statusline_bg },
-        right_sep = { str = '', fg = colors.statusline_bg, bg = colors.statusline_bg },
-        hl = { fg = colors.green, bg = colors.statusline_bg },
-    })
+    -- TODO: Now that LSP progress is in fidget, there's nothing in the middle of this status line.
+    -- Consider moving some stuff here?
 
     table.insert(components.active[2], {
         provider = '',
@@ -509,5 +472,11 @@ return function()
         }
     }
 
+    require('fidget').setup({
+        align = { bottom = false },
+        sources = {
+            ['null-ls'] = { ignore = true },
+        }
+    })
     require('feline').setup(config)
 end
