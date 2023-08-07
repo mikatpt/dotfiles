adddbe3487c01c51631ba7e7bf973b6dd7bc8915
Michael Chen <mikatpt@gmail.com>
2023-08-06 20:32:24 -0400

    bad linter

--- src/.config/nvim/lua/modules/config/comment.lua
+++ src/.config/nvim/lua/modules/config/comment.lua
@@ -3,24 +3,6 @@ return function()
         pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
         padding = true,
         sticky = true,
-        ignore = '',
-        mappings = {
-            basic = true,
-            extra = true,
-        },
-        toggler = {
-            line = 'gcc',
-            block = 'gbc',
-        },
-        opleader = {
-            line = 'gc',
-            block = 'gb',
-        },
-        extra = {
-            above = 'gcO',
-            below = 'gco',
-            eol = 'gcA',
-        },
         post_hook = function() end,
     })
 end
