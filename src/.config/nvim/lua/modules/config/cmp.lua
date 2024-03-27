abb954293c1b4c3f737e422c9c31c4869c64aba9
Michael Chen <mikatpt@gmail.com>
2024-03-27 17:44:08 -0400

    we give up on enter for completion

--- src/.config/nvim/lua/modules/config/cmp.lua
+++ src/.config/nvim/lua/modules/config/cmp.lua
@@ -22,6 +22,12 @@ return function()
         end
     end
 
+    local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    end
+
     local luasnip = require('luasnip')
 
     require('luasnip.loaders.from_vscode').lazy_load({ paths = './snippets' })
@@ -68,6 +74,8 @@ return function()
                     cmp.select_next_item()
                 elseif luasnip.expand_or_jumpable() then
                     luasnip.expand_or_jump()
+                elseif has_words_before() then
+                    cmp.complete()
                 else
                     fallback()
                 end
@@ -85,7 +93,7 @@ return function()
             ['<C-n>'] = cmp.mapping.select_next_item(),
             ['<C-Space>'] = cmp.mapping.complete({}),
             ['<C-e>'] = cmp.mapping.close(),
-            ['<CR>'] = cmp.mapping.confirm({ select = true }),
+            ['<C-y>'] = cmp.mapping.confirm({ select = true }),
         },
         window = {
             border = 'single',
