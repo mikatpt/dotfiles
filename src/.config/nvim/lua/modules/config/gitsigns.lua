8cf38c54fe36749484f0554e2d35ad67ee015acd
Michael Chen <mikatpt@gmail.com>
2024-08-09 16:04:53 -0400

    urgh

--- src/.config/nvim/lua/modules/config/gitsigns.lua
+++ src/.config/nvim/lua/modules/config/gitsigns.lua
@@ -9,14 +9,6 @@ return function()
             changedelete = { text = '~' },
             untracked    = { text = '┆' },
         },
-        signs_staged = {
-            add          = { text = '┃' },
-            change       = { text = '┃' },
-            delete       = { text = '_' },
-            topdelete    = { text = '‾' },
-            changedelete = { text = '~' },
-            untracked    = { text = '┆' },
-        },
         signs_staged_enable = true,
         signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
         numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
