d1efceea5cacbd043b171fc42c3e5c582d85fd08
Michael Chen <mikatpt@gmail.com>
2024-12-07 00:09:25 -0500

    ye

--- src/.config/fish/personal/stats_hook.fish
+++ src/.config/fish/personal/stats_hook.fish
@@ -2,10 +2,6 @@ function testing -e fish_postexec
     # {"command": "argument_here"}
     set -l CMD (jq -n --arg command "$argv" '{$command}')
 
-    # dirty workaround - using jq to escape quotes for us.
-    set -l MUTATION 'mutation { viewer { insertStat(command: '(echo $CMD | jq '.command')') { command updatedAt count } } }'
-    set -l GQL_QUERY (jq -n --arg query $MUTATION '{$query, "variables": {}}')
-
-    # Silent curl to home server
-    curl -H 'Content-Type: application/json' --data-raw "$GQL_QUERY" localhost:7749/graphql 1>/dev/null 2>/dev/null
+    # Silent curl to home server for tracking
+    curl -H 'Content-Type: application/json' --data-raw "$CMD" localhost:7749/stats 1>/dev/null 2>/dev/null
 end
