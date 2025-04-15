db9d0c666dbdbab511a7564f7d26cc76809524ca
Michael Chen <mikatpt@gmail.com>
2025-04-15 11:02:01 -0400

    adjust blink

--- src/.config/nvim/lua/modules/config/blink.lua
+++ src/.config/nvim/lua/modules/config/blink.lua
@@ -1,11 +1,39 @@
 return function()
+    local has_words_before = function()
+        local col = vim.api.nvim_win_get_cursor(0)[2]
+        if col == 0 then
+            return false
+        end
+        local line = vim.api.nvim_get_current_line()
+        return line:sub(col, col):match('%s') == nil
+    end
+
     return {
+        enabled = function()
+            return not vim.tbl_contains(
+                { 'TelescopePrompt', 'NvimTree', 'help', 'lazy', 'mason', 'Trouble', 'notify', 'sagarename' },
+                vim.bo.filetype
+            )
+        end,
         keymap = {
             preset = 'enter',
+            ['<Enter>'] = {
+                function(cmp)
+                    if cmp.is_active() then
+                        return cmp.accept()
+                    else
+                        return cmp.close()
+                    end
+                end,
+                'accept',
+                'fallback',
+            },
             ['<Tab>'] = {
                 function(cmp)
                     if cmp.snippet_active() then
                         return cmp.snippet_forward()
+                    elseif has_words_before() then
+                        return cmp.insert_next()
                     else
                         return cmp.select_next()
                     end
@@ -18,7 +46,7 @@ return function()
                     if cmp.snippet_active() then
                         return cmp.snippet_backward()
                     else
-                        return cmp.select_next()
+                        return cmp.select_prev()
                     end
                 end,
                 'select_prev',
@@ -27,6 +55,17 @@ return function()
         },
         sources = {
             default = { 'lsp', 'path', 'snippets', 'buffer' },
+            providers = {
+                cmdline = {
+                    enabled = function()
+                        return vim.fn.getcmdtype() ~= ':' or not vim.fn.getcmdline():match("^[%%0-9,'<>%-]*!")
+                    end,
+                },
+                buffer = {
+                    enabled = true,
+                    min_keyword_length = 7,
+                },
+            },
         },
         snippets = { preset = 'luasnip' },
         signature = { enabled = false },
@@ -38,8 +77,15 @@ return function()
             },
             list = {
                 selection = {
-                    preselect = function(_)
-                        return not require('blink.cmp').snippet_active({ direction = 1 })
+                    preselect = function(ctx)
+                        if ctx.kind == 'Text' then
+                            P('found text')
+                            return false
+                        end
+                        -- if require('blink.cmp').snippet_active({ direction = 1 }) then
+                        --     return false
+                        -- end
+                        return true
                     end,
                 },
             },
